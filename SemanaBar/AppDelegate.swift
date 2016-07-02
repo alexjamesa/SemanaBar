d8ca4ab15572ae1457e804ea0af895839525674d
Alex Andrews <alexa@thesecretlocation.com>
2016-07-02 08:34:04 -0400

    Initial Commit

---
+++ SemanaBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SemanaBar
+//
+//  Created by U3 on 160702.
+//  Copyright © 2016 All The Ducks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
