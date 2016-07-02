277317f43780aecb1684da0a48d97bf717198f40
Alex Andrews <alexjamesa@gmail.com>
2016-07-02 17:43:34 -0400

    Create single-class version to contrast main branch's "architectured" approach (MVVM)

--- SemanaBar/AppDelegate.swift
+++ SemanaBar/AppDelegate.swift
@@ -13,37 +13,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    let viewModel = ViewModel()
+    // Properties
+    var timer: Timer!
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     
+    // Everything in (almost) one method
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        viewModel.delegate = self
-        setupMenu()
-    }
-    
-    func applicationWillTerminate(_ aNotification: Notification) {
-        viewModel.tearDown()
-    }
-    
-    private func setupMenu(){
+        
+        // Setup status bar item
         statusItem.button!.font = NSFont.menuFont(ofSize: 9)
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "Quit",
                                 action:#selector(quit),
                                 keyEquivalent: "q"))
         statusItem.menu = menu
+        
+        // Setup week-checking timer
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(updateWeek),
+                                     userInfo: nil,
+                                     repeats: true)
+        RunLoop.main().add(timer, forMode: RunLoopMode.commonModes)
+    }
+    
+    // Update week value (called via timer)
+    @objc func updateWeek() {
+        let calender = Calendar.current()
+        let dateComponent = calender.components(Calendar.Unit.weekOfYear,
+                                                from:Date())
+        if let weekNumber = dateComponent.weekOfYear{
+            statusItem.button?.title = "\(weekNumber)"
+        }else{
+            statusItem.button?.title = "...`"
+        }
     }
     
     @objc private func quit(sender:AnyObject){
         NSApplication.shared().terminate(sender)
-    }
-}
-
-
-extension AppDelegate: ViewModelDelegate{
-    
-    /// Update display to newest week number
-    func weekUpdated(_ week: Int?) {
-        statusItem.button?.title = week != nil ? "\(week!)" : ".."
     }
 }
